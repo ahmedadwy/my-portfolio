03f8888a15c8d968d6bfab7c290f43f042f3a7f4
Apple <Apple@MacBook-Pro.local>
2026-07-13 16:01:31 +0300

    add metadata for social sharing

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,23 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Video Editor & Motion Designer Portfolio",
-  description: "Crafting compelling visual narratives",
+  title: 'ADWY | Video Editor & Motion Designer', // اسمك أو اسم البراند
+  description: 'Portfolio of a creative video editor and motion graphics designer.',
+  openGraph: {
+    title: 'ADWY | Creative Portfolio',
+    description: 'Check out my latest video editing and motion graphics projects.',
+    url: 'رابط-موقعك-هنا.vercel.app', // حط اللينك بتاعك اللي طلعته من Vercel
+    siteName: 'ADWY Portfolio',
+    images: [
+      {
+        url: 'https://drive.google.com/file/d/1OFjbFoFLACNj1B-RCiEYOzOZD5uspeIk/view?usp=sharing', // حط رابط صورة كويسة من شغلك هنا
+        width: 1200,
+        height: 630,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({ children }) {
@@ -12,3 +27,4 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
+
